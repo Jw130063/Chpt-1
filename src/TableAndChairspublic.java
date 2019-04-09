35c77be594138338a97df266cece551c86dba195
Joshua Warren <Jw130063@students.davenport.k12.ia.us>
2019-04-09 12:30:04 -0500

    Update

---
+++ src/TableAndChairspublic.java
@@ -0,0 +1,34 @@
+public class TableAndChairspublic{
+    //Author: Warren
+    //date: 04/09/19
+    //Project: Table and chairs
+
+    static void main(String[] args){
+
+
+        System.out.println("Building Some Tables And Chairs");
+        System.out.println("\n\n\n\n");
+
+        System.out.println("C                                                    C");
+        System.out.println("H                                                    H");
+        System.out.println("A                                                    A");
+        System.out.println("I                  TABLETABLETABLE                   I");
+        System.out.println("R                     L        L                     R");
+        System.out.println("CHAIRCHAIR            E        E              CHAIRCHAIR");
+        System.out.println("C        C            G        G              C      C");
+        System.out.println("H        H            L        L              H      H");
+        System.out.println("A        A            E        E              A      A");
+        System.out.println("I        I            G        G              I      I");
+        System.out.println("R        R            !        !              R      R");
+
+
+
+
+
+    }
+}
+
+
+
+
+
