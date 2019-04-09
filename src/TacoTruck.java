41d651e0708b3ad7dd3e5d7bde30049300221558
Joshua Warren <Jw130063@students.davenport.k12.ia.us>
2019-04-09 13:21:17 -0500

    update

---
+++ src/TacoTruck.java
@@ -0,0 +1,29 @@
+public class TacoTruck {
+    public static void main(String[] args){
+
+        System.out.println("TacoTruck");
+        System.out.println("\n\n\n\n");
+
+        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        System.out.println("x                                       x");
+        System.out.println("x        TACO TRUCK                     x");
+        System.out.println("x       xxxxxxxxxxxxxxxxxxxxx           x");
+        System.out.println("x       x                   x             xxxxxxxxx");
+        System.out.println("x       x                   x                     x");
+        System.out.println("x       xxxxxxxxxxxxxxxxxxxxx                     x");
+        System.out.println("x                                                 x");
+        System.out.println("x    xxxxx                         xxxxx          x");
+        System.out.println("xxxxx    xxxxxxxxxxxxxxxxxxxxxxxxxx     xxxxxxxxxx");
+        System.out.println("    o    o                         o    o");
+        System.out.println("     oooo                           oooo    ");
+
+    
+
+
+
+
+
+
+
+    }
+}
