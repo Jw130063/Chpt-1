ed7b7f9cf099761c88301a33961d40420d73595b
Joshua Warren <Jw130063@students.davenport.k12.ia.us>
2019-04-11 12:20:43 -0500

    update debugging

---
+++ src/FixDebugOne.java
@@ -0,0 +1,9 @@
+public class FixDebugOne
+{
+    /* This program displays a greeting */
+    public static void main(String[] arg)
+    {
+        System.out.println("Hello");
+        // put semicolan in
+    }
+}
---
+++ src/FixDebugOne4.java
@@ -0,0 +1,7 @@
+public class FixDebugOne4
+{
+    public static void main(String[] args)
+    {
+        JoptionPane.showMessageDialog(null, "First GUI program");
+    }
+}
